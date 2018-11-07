b38da9db504e641145b4de3b26c3a06d6a386adb
Jose Victor Hendz Martins <Jose>
2018-11-07 20:52:42 -0200

    Test

--- src/Controllers/test-controller.js
+++ src/Controllers/test-controller.js
@@ -1,6 +1,13 @@
+exports.test1 = async (request, res) => {
+  const testParams = request.params
+  const testBody = request.body
+  console.log("--------------------", testParams, testBody)
+  res.json({value:'working test'});
+};
+
 exports.test = async (request, res) => {
   const testParams = request.params
   const testBody = request.body
   console.log("--------------------", testParams, testBody)
   res.json({value:'working test'});
-}; 
+};
--- src/Routes/test-route.js
+++ src/Routes/test-route.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../Controllers/test-controller');
 
+router.get('/teste1', controller.test1);
 router.post('/', controller.test);
 
 module.exports = router;
