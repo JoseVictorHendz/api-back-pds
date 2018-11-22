b687158f012ed7f186041d5ccbaee88c49c600ab
Jose Victor Hendz Martins <Jose>
2018-11-22 18:38:39 -0200

    Return colors in ptbr

--- src/utils/colors-utils.js
+++ src/utils/colors-utils.js
@@ -1,18 +1,24 @@
+const ntc = require('../utils/ntc')
+
 exports.parseRgbForColorName = async (dataRgb) => {
-    let colorName = []
+    let colorNameUS = []
+    let colorNamePtBr = []
+
+    let color = ""
 
     for(const element of dataRgb) {
-        colorName.push(await fullColorHex(element.red, element.green, element.blue))
+        color = await ntc.ntcMatch(await fullColorHex(element.red, element.green, element.blue))
+        colorNameUS.indexOf(color) < 0 ? colorNamePtBr.push(translateForPtBr(color)) : false
+        colorNameUS.push(color)
     }
-
-    return colorName
+    return colorNamePtBr
 }
 
 function fullColorHex(r,g,b) {   
     var red = rgbToHexadecimal(r);
     var green = rgbToHexadecimal(g);
     var blue = rgbToHexadecimal(b);
-    return red+green+blue;
+    return "#"+red+green+blue;
   };
 
 function rgbToHexadecimal(rgb) {
@@ -23,6 +29,21 @@ function rgbToHexadecimal(rgb) {
     return hex;
 }
 
-// function identificColor(hexadecimal) {
-//     return colorName
-// }
+function translateForPtBr(color) {
+    switch(color) {
+        case "Red": color = "Vermelho"; break
+        case "Green": color = "Verde"; break
+        case "Blue": color = "Azul"; break
+        case "Yellow": color = "Amarelo"; break
+        case "Violet": color = "violeta"; break
+        case "Brown": color = "Marrom"; break
+        case "Black": color = "Preto"; break
+        case "White": color = "Branco"; break
+        case "Gray": color = "Cinza"; break
+        case "Orange": color = "Laranja"; break
+
+        default: color = "cor nao identificada " +color
+    }
+    return color
+}
+
